11ae2ae12989715dc71d9b750266bed4c9eaa3d3
Sam <sam.pierce76@gmail.com>
2024-02-11 11:58:57 -0500

    Clean up old commands

--- extension.js
+++ extension.js
@@ -1,34 +1,16 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 const vscode = require("vscode");
-
-// The module 'azdata' contains the Azure Data Studio extensibility API
-// This is a complementary set of APIs that add SQL / Data-specific functionality to the app
-// Import the module and reference it with the alias azdata in your code below
-
-// Note: uncomment when you want to use Azure Data Studio APIs. Commenting now to avoid strict linting issues
 const azdata = require("azdata");
 
 const jsdom = require("jsdom");
 const { JSDOM } = jsdom;
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 function activate(context) {
-  // Use the console to output diagnostic information (console.log) and errors (console.error)
-  // This line of code will only be executed once when your extension is activated
   console.log(
     'Congratulations, your extension "interface-rule-tool" is now active!'
   );
 
-  // The command has been defined in the package.json file
-  // Now provide the implementation of the command with  registerCommand
-  // The commandId parameter must match the command field in package.json
   context.subscriptions.push(
     vscode.commands.registerCommand("interface-rule-tool.helloWorld", () => {
-      // The code you place here will be executed every time your command is executed
-
-      // Display a message box to the user
       vscode.window.showInformationMessage("Hello World! Mwuahaha");
     })
   );
@@ -37,9 +19,6 @@ function activate(context) {
     vscode.commands.registerCommand(
       "interface-rule-tool.showCurrentConnection",
       () => {
-        // The code you place here will be executed every time your command is executed
-
-        // Display a message box to the user
         azdata.connection.getCurrentConnection().then(
           (connection) => {
             let connectionId = connection
@@ -56,24 +35,27 @@ function activate(context) {
   );
 
   context.subscriptions.push(
-    vscode.commands.registerCommand("interface-rule-tool.removeXML", () => {
-      const activeEditor = vscode.window.activeTextEditor;
-      if (!activeEditor) {
-        vscode.window.showInformationMessage("activeEditor not found");
-      } else {
-        const newEditorContent = parseSQLFromXML(activeEditor);
-        newTextEditor()
-          .then(() => {
-            const newTextEditor = vscode.window.activeTextEditor;
-            newTextEditor.insertSnippet(
-              new vscode.SnippetString(newEditorContent)
-            );
-          })
-          .then(() => {
-            vscode.commands.executeCommand("editor.action.formatDocument");
-          });
+    vscode.commands.registerCommand(
+      "interface-rule-tool.extractSQLFromXML",
+      () => {
+        const activeEditor = vscode.window.activeTextEditor;
+        if (!activeEditor) {
+          vscode.window.showInformationMessage("activeEditor not found");
+        } else {
+          const newEditorContent = parseSQLFromXML(activeEditor);
+          newTextEditor()
+            .then(() => {
+              const newTextEditor = vscode.window.activeTextEditor;
+              newTextEditor.insertSnippet(
+                new vscode.SnippetString(newEditorContent)
+              );
+            })
+            .then(() => {
+              vscode.commands.executeCommand("editor.action.formatDocument");
+            });
+        }
       }
-    })
+    )
   );
 }
 
